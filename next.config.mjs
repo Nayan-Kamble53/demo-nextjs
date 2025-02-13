c88a10f7f4696e294ad2d9bf3c56f7eb26687bf7
Nayan <nayankamble712@gmail.com>
2025-02-13 12:31:05 +0530

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -3,18 +3,37 @@
 
 // export default nextConfig;
 
+// /** @type {import('next').NextConfig} */
+// import withPWA from 'next-pwa';
+// const nextConfig = {
+//     distDir: "build",
+//     reactStrictMode: true,
+//     compiler: {
+//         removeConsole: process.env.NODE_ENV !== "development",
+//     },
+// };
+
+// export default withPWA({
+//     dest: "public",
+//     disable: process.env.NODE_ENV === 'development',
+//     register: true,
+//     skipWaiting: true,
+// })(nextConfig);
+
+
 /** @type {import('next').NextConfig} */
 import withPWA from 'next-pwa';
+
 const nextConfig = {
-    distDir: "build",
     reactStrictMode: true,
     compiler: {
         removeConsole: process.env.NODE_ENV !== "development",
     },
+    output: "standalone", 
 };
 
 export default withPWA({
-    dest: "public",
+    dest: "public", 
     disable: process.env.NODE_ENV === 'development',
     register: true,
     skipWaiting: true,
