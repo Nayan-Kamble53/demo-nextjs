a2d4a23512b73f1d3a809dd908304c40c0476a56
Nayan <nayankamble712@gmail.com>
2025-02-13 17:05:37 +0530

    made changes for mobile devices

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,4 @@
-"use client"; // Ensure this runs only on the client side
-
+"use client"; 
 import { useEffect, useState } from "react";
 import Link from "next/link";
 import "../app/globals.css";
@@ -11,8 +10,10 @@ export default function Home() {
   useEffect(() => {
     const handleBeforeInstall = (e) => {
       e.preventDefault(); 
-      setDeferredPrompt(e); 
-      setShowInstallBtn(true); 
+      if (window.innerWidth < 1285) {
+        setDeferredPrompt(e);
+        setShowInstallBtn(true);
+      } 
     };
 
     window.addEventListener("beforeinstallprompt", handleBeforeInstall);
@@ -36,6 +37,12 @@ export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center gap-5 p-24">
       <h1 className="font-bold text-2xl">This is home page...</h1>
+      <div className="text-blue-600 underline">
+        <Link href="/about">About Page</Link> <br />
+        <Link href="/contact">Contact Page</Link> <br />
+        <Link href="/blogs">Blogs Page</Link> <br />
+        <Link href="/products">Products Page</Link>
+      </div>
 
       {showInstallBtn && (
         <button 
@@ -45,13 +52,6 @@ export default function Home() {
           Install App
         </button>
       )}
-
-      <div className="text-blue-600 underline">
-        <Link href="/about">About Page</Link> <br />
-        <Link href="/contact">Contact Page</Link> <br />
-        <Link href="/blogs">Blogs Page</Link> <br />
-        <Link href="/products">Products Page</Link>
-      </div>
     </div>
   )
-}
+} 
